317a75011ba52d63ac3a5427cb903234a8e3b37d
Mateus-Cordeiro <mateusvcordeiro@gmail.com>
2015-12-09 16:56:47 +0000

    Project from Android Studio

---
+++ app/src/test/java/com/computer_networks/aalto_tm/heatupyoursmartphone/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.computer_networks.aalto_tm.heatupyoursmartphone;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
